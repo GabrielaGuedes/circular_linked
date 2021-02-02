de43fe315d742f726c61fd95853bc1675fa7f18e
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2021-02-01 21:24:39 -0300

    create node

---
+++ lib/circular-linked-list/node.rb
@@ -0,0 +1,11 @@
+module CircularLinkedList
+  class Node
+
+    attr_accessor :value, :next
+
+    def initialize(value, next_node = nil)
+      @value = value
+      @next = next_node
+    end
+  end
+end
---
+++ spec/circular-linked-list/node_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../../lib/circular-linked-list/node.rb'
+
+RSpec.describe CircularLinkedList::Node do
+  describe "#initialize" do
+    let(:value) { "Value" }
+
+    context "when there is no next_node" do
+      let(:value) { "Value" }
+      subject(:new_node) { described_class.new(value) }
+
+      it "saves the params passed" do
+        expect(new_node.value).to eq(value)
+        expect(new_node.next).to eq(nil)
+      end
+    end
+
+    context "when there is next_node" do
+      let(:next_node) { described_class.new("Next node value")}
+      subject(:new_node) { described_class.new(value, next_node) }
+
+      it "saves the params passed" do
+        expect(new_node.value).to eq(value)
+        expect(new_node.next).to eq(next_node)
+      end
+    end
+  end
+end
